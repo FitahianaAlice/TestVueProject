ec10dfbe5eeae146058cbd4473a38daae4602c39
Fitahiana Valisoa <fitahianaalice@gmail.com>
2024-02-27 12:47:08 +0300

    Adding Navbar in HomeView.vue

--- vue-project-test/src/views/HomeView.vue
+++ vue-project-test/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import NavbarView from './NavbarView.vue';
 </script>
 
 <template>
   <main>
+    <NavbarView />
     <TheWelcome />
   </main>
 </template>
