6f34142f3a56a3b8fd24f47177c9c6f3873c5c6d
Fitahiana Valisoa <fitahianaalice@gmail.com>
2024-02-28 12:01:31 +0300

    Styling the deconnexion page

--- vue-project-test/src/views/DeconnexionView.vue
+++ vue-project-test/src/views/DeconnexionView.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-      <h1>Merci et à bientôt !</h1>
-      <button @click="logout">Déconnexion</button>
+    <div class="center-content">
+      <h1>Voulez-vous vraiment vous déconnecter ?</h1>
+      <button @click="logout">Oui</button>
+      <button @click="notlogout">Non</button>
     </div>
   </template>
   
@@ -22,8 +23,43 @@
         // Effacer les cookies, effacer les données de session, etc.
         // Rediriger l'utilisateur vers la page de connexion
         this.$router.push({ name: 'authentification' });
+      },
+      notlogout() {
+        // Code pour déconnecter l'utilisateur, par exemple :
+        // Effacer les cookies, effacer les données de session, etc.
+        // Rediriger l'utilisateur vers la page de connexion
+        this.$router.push({ name: 'Dashboard' });
       }
     }
   };
   </script>
   
+  <style scoped>
+  body {
+      font-family: Arial, sans-serif;
+  }
+  h1 {
+      color: #333; 
+      font-size: 24px; 
+      margin-bottom: 20px; 
+  }
+  button {
+      background-color: #007bff; 
+      color: white;
+      margin: 1%; 
+      padding: 10px 20px; 
+      border: none; 
+      cursor: pointer; 
+    }
+  button:hover {
+      background-color: #0056b3;
+  }
+  .center-content {
+    display: flex; 
+    justify-content: center;
+    align-items: center; 
+    min-height: 100vh; 
+    text-align: center; 
+    flex-direction: column;
+  }
+</style>
